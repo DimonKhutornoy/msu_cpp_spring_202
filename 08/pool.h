96ba8e3572eae6ad4bdb3b1a45f31d7465656d93
DimonKhutornoy <dimon.khutornoy@mail.ru>
2020-04-25 17:26:59 +0300

    Project with questions

--- 08/pool.h
+++ 08/pool.h
@@ -9,48 +9,53 @@
 
 class ThreadPool
 {
-    size_t size; 
+    size_t size;
     bool active;
 
-    std::queue <std::function<void()>> queue_;    //очередь функциональных объектов, тут будем хранить процессы на выполнение
-    std::queue <std::thread> pool_;               //наш pool
+    std::queue<std::function<void()>>
+        queue_; //очередь функциональных объектов, тут будем хранить процессы на выполнение
+    std::queue<std::thread> pool_; //наш pool
 
     std::mutex mut;
     std::condition_variable cv;
 
 public:
     ThreadPool(size_t poolSize)
-        : size (poolSize)
+        : size(poolSize)
         , active(true)
     {
         for (size_t i = 0; i < size; i++)
         {
-            pool_.push(std::thread([this]() {                //пушим в наш pool потоки, в которых выполняются наши функции
-                while (true)                                 //будет выполняться до тех пор, пока не опустеет очередь
-                {
-                    std::unique_lock<std::mutex> lock(mut);  //замок для того, чтобы безопасно вытащить функцию из очереди
-                    if (!queue_.empty())
+            pool_.push(std::thread([this]() //пушим в наш pool потоки, в которых выполняются наши функции
+                { 
+                    while (true) //будет выполняться до тех пор, пока не опустеет очередь и не запустится деструктор
                     {
-                        auto func = std::move(queue_.front());  
-                        queue_.pop();                           //забираем функцию из очереди
-                        lock.unlock();                          //разрешаем работать другим потокам
-                        func();                                 //выполняем функцию из потока
+                        std::unique_lock<std::mutex> lock(
+                            mut); //замок для того, чтобы безопасно вытащить функцию из очереди
+                        if (!queue_.empty())
+                        {
+                            auto func = std::move(queue_.front());
+                            queue_.pop(); //забираем функцию из очереди
+                            lock.unlock(); //разрешаем работать другим потокам
+                            func(); //выполняем функцию из потока
+                        }
+                        else
+                        {
+                            if (active)
+                                cv.wait(lock); //Ложимся спать, ждём задач
+                            else
+                                break; //Выходим, если pool был убит и список задач пуст
+                        }
                     }
-                    else
-                    {
-                        if (active) cv.wait(lock);              //Ложимся спать, ждём задач
-                        else break;                             //Выходим, если pool был убит и список пуст
-                    }
-                }
-            }));
+                }));
         }
     }
 
-    ~ThreadPool ()
+    ~ThreadPool()
     {
         active = false;
-        cv.notify_all();          //Wake up
-        while (!pool_.empty())    //And die
+        cv.notify_all(); // Wake up
+        while (!pool_.empty()) // And die
         {
             pool_.front().join();
             pool_.pop();
@@ -60,18 +65,19 @@ public:
     template <class Func, class... Args>
     auto exec(Func func, Args... args) -> std::future<decltype(func(args...))>
     {
-        auto p = std::make_shared < std::packaged_task < typename std::result_of<Func(Args...)>::type() >> //Работает только с shared, иначе никак. Почему???
-		(std::bind(std::forward<Func>(func), std::forward<Args>(args)...));  //делаем для функции тот интерфейс, который требуется
+        auto p = std::make_shared<std::packaged_task<typename std::result_of<Func(Args...)>::type()>> //Работает только с shared, иначе никак. Почему???
+            (std::bind(std::forward<Func>(func), std::forward<Args>(args)...)); //делаем для функции тот интерфейс, который требуется
+        std::lock_guard<std::mutex> lock(mut); //Не знаю, нужен ли он тут, но пусть пока будет???
+        if (!active)
         {
-            std::lock_guard<std::mutex> lock(mut);                           //Не знаю, нужен ли он тут, но пусть пока будет???
-			if (!active)
-			{
-				throw std::runtime_error("The pool has stopped");           //Для безопасности
-			}
-			queue_.push([p]() { (*p)(); });                                 //добавляем в очередь.
-        };
-        cv.notify_one();                                                    //будим любой поток, пусть выполняет нашу задачу
-        return p->get_future();                                             
+            throw std::runtime_error("The pool has stopped"); //Для безопасности
+        }
+        queue_.push([p]()
+            {
+                (*p)();
+            }); //добавляем в очередь. Нельзя ли никак это более адекватно реализовать???
+        cv.notify_one(); //будим любой поток, пусть выполняет нашу задачу
+        return p->get_future();
     }
 };
 
--- 08/test.cpp
+++ 08/test.cpp
@@ -2,22 +2,38 @@
 #include <iostream>
 
 
-struct A { int data=3; };
-void foo(const A& a) { std::cout << a.data << std::endl; }
-
-int main (int argc, char* argv[])
+struct A
 {
-	ThreadPool pool(8);
+    int data = 3;
+};
+void foo(const A& a)
+{
+    std::cout << a.data << std::endl;
+}
 
-	auto task1 = pool.exec(foo, A());
-	task1.get();
+int main(int argc, char* argv[])
+{
+    ThreadPool pool(8);
 
-	auto task2 = pool.exec([]() { return 1; });
-	std::cout << task2.get() << std::endl;
-	
-	std::cout<<'\n';
-	
-    for (int i = 0; i <= 20; ++i) {
-        std::cout<<pool.exec([i]() { return i; }).get() << '\n';
+    auto task1 = pool.exec(foo, A());
+    task1.get();
+
+    auto task2 = pool.exec([]()
+        {
+            return 1;
+        });
+    std::cout << task2.get() << std::endl;
+
+    std::cout << '\n';
+
+    for (int i = 0; i <= 20; ++i)
+    {
+        std::cout << pool.exec([i]()
+                             {
+                                 return i;
+                             })
+                         .get()
+                  << '\n';
     }
 }
+
